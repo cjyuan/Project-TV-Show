3b71cff710aa532ecb01bb32a7f83efe347afcb9
cjyuan <cjyuan00.gmail.com>
2025-01-20 00:57:29 +0000

    implementation of level-100

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+// Construct the HTML element for each episode
+function constructEpisodeElement({name, season, number, image : {medium}, summary}) {
+
+  const episodeCode = `S${season.toString().padStart(2, "0")}E${number.toString().padStart(2, "0")}`;
+
+  const episodeDiv = document.createElement("div");
+  episodeDiv.className = "episode";
+
+  // Trade convenience for performance (can be replaced if needed)
+  episodeDiv.innerHTML = `
+  <div class="epName">${name}<div>
+  <div class="epCode">${episodeCode}</div>
+  <div class="epImage"><img src="${medium}" alt="Episode image"></div>
+  <div class="epSummary">${summary}</div>  
+  `;
+
+  return episodeDiv;
 }
 
-window.onload = setup;
+
+function makePageForEpisodes(episodeList) {
+
+  const rootEle = document.getElementById("root");
+
+  rootEle.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    rootEle.appendChild(constructEpisodeElement(episode));
+  });
+
+}
+
+
+window.addEventListener("load", setup);
